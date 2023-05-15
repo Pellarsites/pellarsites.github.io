b86a7137912c8b346612247b4fc3a9dff24769c0
Roman Garcia <119982112+ro2alive@users.noreply.github.com>
2023-05-15 13:35:52 -0500

    Create home.js

---
+++ home.js
@@ -0,0 +1,32 @@
+var says = 
+[
+    "uh",
+    "my coder friends copying me and hes better at coding then me",
+    "Schools Pretty Boring",
+    "This Websites Trash", 
+    "i took too long making this :(", 
+    "AOSUDHASIUD4u3ghiuj34hgi34gH", 
+    "tinyurl.com/RailinDev", 
+    "I stole 3kh0's idea to make a website >:)", 
+    "hotline bling", 
+    "all 1v1lol's are blocked at my school", 
+    "the person who made this websites pretty cool", 
+    "what day is it?", 
+    "fortnite battl pass", 
+    "i made this on the weekend :((((",
+    "hiiii aiden :)",
+    "i hate myself",
+    "im in love with a emo gorl 💅",
+    "amog",
+    "💀",
+    "hey e-kitten",
+    "rip 3kh0"
+]
+
+function pageLoad() {
+    document.getElementById('randomting').innerHTML = says[Math.floor(Math.random() * says.length)]
+}
+
+if (window.location.href.match("cartxrr")) {
+    alert("if you have ad blocker on please disable it! it helps us run the website thanks 💓");
+}
